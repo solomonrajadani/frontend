5b3198f6e656765deffbf740c8998d37cb730f03
solomonrajadani <147127024+solomonrajadani@users.noreply.github.com>
2023-10-16 11:37:58 +0530

    added "frontend"

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function togglePortfolioSubMenu() {
+    var subMenu = document.getElementById('portfolioSubMenu');
+    subMenu.style.display = subMenu.style.display === 'block' ? 'none' : 'block';
+}
+
+
